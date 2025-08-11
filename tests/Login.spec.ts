6df66999d7b498904019e6ce9792e10393115c2f
Bryan Rojas <brxanete@outlook.com>
2025-08-10 22:50:45 -0500

    Avance demoblaze

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -7,19 +7,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const mailTextBox = (page) => page.getByTestId('royal-email');
-const passTextBox = (page) => page.getByTestId('royal-pass');
-const loginButton = (page) => page.getByTestId('royal-login-button');
+const mainBanner = (page) => page.getByRole('link', { name: 'PRODUCT STORE' });
+const homeButton = (page) => page.getByRole('link', { name: 'Home (current)' });
+const contactButton = (page) => page.getByRole('link', { name: 'Contact' });
+const loginButton1 = (page) => page.getByRole('link', { name: 'Log in' });
+const mailTextBox = (page) => page.locator('#loginusername');
+const passTextBox = (page) => page.locator('#loginpassword');
+const loginButton2 = (page) => page.getByTestId('royal-login-button');
 const forgotPassButton = (page) => page.getByRole('link', { name: 'Forgot password?' });
 const checkNotificationsTitle = (page) => page.getByText('Revisa las notificaciones');
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://facebook.com/');
+  await page.goto('https://www.demoblaze.com');
 });
 
 test('has title', async ({ page }) => {
   test.step('Validar que el titulo de la pagina sea el correcto', async () => {
-    await expect(page).toHaveTitle("Facebook - log in or sign up");
+    await expect(page).toHaveTitle("STORE");
   })
 
 });
@@ -28,14 +32,14 @@ test('has title', async ({ page }) => {
 test('validate visual elements on home page', async ({ page }) => {
 
   await test.step('Validar el cajon de texto para email', async () => {
-  await expect(mailTextBox(page)).toBeVisible();
+  await expect(mainBanner(page)).toBeVisible();
   await expect(mailTextBox(page)).toBeEditable();
   await expect(mailTextBox(page)).toBeEmpty();
   });
 
 
   await test.step('Validar el cajon de texto para contrasena', async () =>{
-    
+
   })
 });
 
@@ -47,6 +51,11 @@ test('do a login', async ({ page }) => {
   if (!process.env.FB_PASSWORD) {
     throw new Error('FB_PASSWORD is not defined in environment variables');
   }
+
+  await test.step('Ir a la seccion de login', async () => {
+    await expect(loginButton1(page)).toBeVisible();
+    await loginButton1(page).click();
+  });
 
   await test.step('Llenar email y capturar screenshot', async () => {
     await mailTextBox(page).fill(process.env.FB_EMAIL);
@@ -60,6 +69,21 @@ test('do a login', async ({ page }) => {
   await test.step('Hacer clic en login', async () => {
     await loginButton(page).click();
   });
+
+  await test.step('Validar aviso notificaciones', async() => {
+    await expect(checkNotificationsTitle(page)).toBeVisible();
+  });
+
+  await test.step('validar', async () => {
+
+  });
+
+
+
+
+
+  
+
 
 
 
